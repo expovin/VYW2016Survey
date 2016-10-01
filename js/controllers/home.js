312396d68b44d90f4187c7df4e680af2d826beab
QlikService <QlikService>
2016-10-01 23:17:57 +0200

    Finalizzazione loop chart

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -11,15 +11,17 @@
 app.obj.angularApp
 	.controller('controller.home', function ($scope, $rootScope, $location, $injector, $interval, api, utility) {
 
-		var cont = -1, rDim=0, rFiltro = 0, rType=0;
+		var contFamily = [-1,-1,-1], Family=0, count=-1,
+		    rDim1=0, rDim2=0, 
+		    rFiltro = 0, rType=0,
+		    rOraria=0, lOraria=3;
 		$scope.chart={};
 	   //Put in interval, first trigger after 10 seconds 
 
 
 		var chartsLibrary = {
 			"appId": "b74e2ae5-4cda-402d-bccc-55b2e162cb6c",
-			"chart" : {
-				
+			"chartFamily" : [{
 				"type" : ["barchart"],
 				"dimensioni" : ["JobLevel","JobFunction","Lead.Industry"],
 				"misure" : "Count({<text={'$CAMPO$'}>}text)/Count({<Profilo={'MILLENNIAL','HIPPIE','SNOB','NERD'}>}Profile) * 100",
@@ -47,27 +49,86 @@ app.obj.angularApp
 							 ["ConfCall","Sfruttiamo la tecnologia ed evitiamo movimenti, meglio le conf call"],
 							 ["Vela","Meglio la vela"],
 							 ["Yacht","Meglio lo Yacht"]
-							]
-			}
+							],
+				"options": {
+								"orientation" : "horizontal",
+								"dataPoint": {"showLabels": true } ,
+								"dimensionAxis": {"show": "all" } ,
+								"measureAxis": {"show": "none" } ,
+								"color": {"auto": false , "mode": "byMeasure" }
+							}
+			},
+			{
+				"type" : ["piechart"],
+				"dimensioni" : ["JobLevel","JobFunction","Lead.Industry"],
+				"misure" : "Count({<Profile={'$CAMPO$'}>}Profile)",
+				"title" :  "Chi preferisce il ",
+				"filtro" : [
+					["HIPPIE","Come sono distribuiti gli Hippie?"],
+					["MILLENNIAL","Dove sono i Millennial"],
+					["NERD","Scopriamo chi è Nerd"],
+					["SNOB","Ecco gli Snob"]
+				],
+				"options": {
+								"donut": {"showAsDonut": true } ,
+								"dataPoint": {"showLabels": true } ,
+								"dimensionAxis": {"show": "all" } ,
+								"measureAxis": {"show": "none" } ,
+								"color": {"auto": false , "mode": "byMeasure" }
+							}
+			},
+			{
+				"type" : ["barchart"],
+				"dimensioni" : ["ora"],
+				"misure" : "Count({<Profile={'MILLENNIAL','SNOB','HIPPIE','NERD'}>}Profile)",
+				"title" :  "Chi preferisce il ",
+				"filtro" : [
+					["Falso","Distribuzione oraria"]
+				],
+				"options": {
+								"orientation" : "vertical",
+								"dimensionAxis": {"show": "all" } ,
+								"measureAxis": {"show": "none" } ,
+								"color": {"auto": false , "singleColor": 5 }
+							}
+			},
+			]
 
 		};
 
+
 	   var theInterval = $interval(function(){
-	    	
-	      	cont += 1;
-	      	rDim = cont % chartsLibrary.chart.dimensioni.length;
-	      	rFiltro = cont % chartsLibrary.chart.filtro.length;
-	      	rType = cont % chartsLibrary.chart.type.length;
-	      	$scope.chart.titolo = chartsLibrary.chart.filtro[rFiltro][1];
+	    	$scope.chart={}
+
+	    	count +=1;
+	    	Family = count % chartsLibrary.chartFamily.length;
+	      	contFamily[Family] += 1;
+	      	rOraria = count % lOraria;
+	      	if(rOraria)
+	      	    $scope.chart.Loop = false;
+	      	else
+	      		$scope.chart.Loop = true;
+
+	      	rDim1 = contFamily[Family] % chartsLibrary.chartFamily[Family].dimensioni.length;
+	      	rDim2 = (contFamily[Family]+1) % chartsLibrary.chartFamily[Family].dimensioni.length;
+	      	rFiltro = contFamily[Family] % chartsLibrary.chartFamily[Family].filtro.length;
+	      	rType = contFamily[Family] % chartsLibrary.chartFamily[Family].type.length;
+	      	$scope.chart.titolo = chartsLibrary.chartFamily[Family].filtro[rFiltro][1];
 
 	      	$scope.chart.appId = chartsLibrary.appId;
-	      	$scope.chart.Type= chartsLibrary.chart.type[rType];
-	      	$scope.chart.dimensione = chartsLibrary.chart.dimensioni[rDim];
-	      	var str = chartsLibrary.chart.misure
-	      	$scope.chart.misura = str.replace("$CAMPO$",chartsLibrary.chart.filtro[rFiltro][0]);
+	      	$scope.chart.Type= chartsLibrary.chartFamily[Family].type[rType];
+	      	$scope.chart.dimensione1 = chartsLibrary.chartFamily[Family].dimensioni[rDim1];
+	      	$scope.chart.dimensione2 = chartsLibrary.chartFamily[Family].dimensioni[rDim2];
+	      	var str = chartsLibrary.chartFamily[Family].misure
+	      	$scope.chart.misura = str.replace("$CAMPO$",chartsLibrary.chartFamily[Family].filtro[rFiltro][0]);
+	      	$scope.chart.options = chartsLibrary.chartFamily[Family].options;
+
+	      	
+
+	      	console.log($scope.chart);
+
 	      	
 	   }.bind(this), 15000); 
-
 
 
 
@@ -94,13 +155,15 @@ app.obj.angularApp
 			//utility.log('Page loaded: ', $scope.page);
 
 
-
 			//get objects -- inserted here --
-			app.obj.app.getObject('CHART1','NrNAR');
+			
+			app.obj.app.getObject('CHART1_ORARIO','EmatcB');
+			/*
 			app.obj.app.getObject('CHART2','Fyksmc');
+
 			app.obj.app.getObject('CHART3','ACSSvY');
 			app.obj.app.getObject('CHART4','RUHEftG');
-
+*/
 
 			//var prof={};
 			
